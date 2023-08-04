c8e3d8e9d2d615d70a2921894be75ed272d8ae74
Kailash <ahikailash1@gmail.com>
2023-08-04 19:03:10 +0800

    integration: tailwindcss and flowbite

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import AigenLogo from "./launch/assets/aigen-logo-light.svg"
 import './App.css';
+import { Button } from 'flowbite-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={AigenLogo} alt="Aigen Logo" />
-        Aigen Launchpad
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={AigenLogo} alt="Aigen Logo"/>
+                <div className="text-3xl font-bold underline my-4">Aigen Launchpad</div>
+                <Button> Coming Soon... </Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
